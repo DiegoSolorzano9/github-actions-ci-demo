5afb3761c02112f7d94dc516a96dc8144ab8ba24
Diego Solorzano <solorzano.arancibia.diegosantiago@usfx.bo>
2026-05-10 19:49:53 -0400

    ci: agrega paso de linting al pipeline

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,28 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-]);
+export default [
+  js.configs.recommended,
+  {
+    // Configuramos cómo se deben tratar los archivos
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module", // Esto permite el 'import/export' en este archivo
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    rules: {
+      "no-undef": "error",
+      "no-unused-vars": "warn",
+    },
+  },
+  {
+    // Regla específica para tus archivos .js de Node (CommonJS)
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+    },
+  }
+];
